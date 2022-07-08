3dd720eb28f3f4484ef0eac0a7783dc3fb068b95
Harsh Kumar <95271790+harshrounder@users.noreply.github.com>
2022-07-08 18:08:17 +0530

    Add files via upload
    This is a music player android app named Yay! Music . It can play all the songs you want. I have attached all the code that was has been used to build this android app.

---
+++ MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.yaymusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listViewId);
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+//                        Toast.makeText(MainActivity.this, "RunTIme permission GIven", Toast.LENGTH_SHORT).show();
+                        ArrayList<File> mySongs = fetchSongs(Environment.getExternalStorageDirectory());
+                        String [] items = new String[mySongs.size()];
+                        for(int i=0;i<mySongs.size();i++){
+                            items[i] = mySongs.get(i).getName().replace(".mp3","");
+                        }
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,items);
+                        listView.setAdapter(adapter);
+
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                                Intent intent = new Intent(MainActivity.this, PlaySong.class);
+                                String currentSong = listView.getItemAtPosition(position).toString();
+                                intent.putExtra("songList",mySongs);
+                                intent.putExtra("currentSong",currentSong);
+                                intent.putExtra("position",position);
+                                startActivity(intent);
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(MainActivity.this, "Please give permission to use the services.", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+
+
+
+
+    }
+
+    public ArrayList<File> fetchSongs(File file){
+        ArrayList arrayList = new ArrayList();
+        File [] songs = file.listFiles();
+        if(songs !=null){
+            for(File myfile: songs){
+                if(myfile.isDirectory() && !myfile.isHidden()) {
+                    arrayList.addAll(fetchSongs(myfile));
+
+                }
+                else{
+                    if(myfile.getName().endsWith(".mp3") && !myfile.getName().startsWith(".")){
+                        arrayList.add(myfile);
+                    }
+                }
+            }
+        }
+        return arrayList;
+    }
+
+}
